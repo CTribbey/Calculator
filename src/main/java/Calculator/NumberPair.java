9ab5aac0f0a5f767b101e2ad477d9723263b47e0
Aether <7040708+CTribbey@users.noreply.github.com>
2020-09-23 15:44:32 -0500

    Initial commit

---
+++ src/main/java/Calculator/NumberPair.java
@@ -0,0 +1,22 @@
+package Calculator;
+
+public class NumberPair {
+    private double x = 0;
+    private double y = 0;
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
